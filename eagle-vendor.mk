b01327f4b132ad962f68a46d60bea7b276d3285b
J. Daniel Medeiros <jdmedeiros@gmail.com>
2015-03-19 13:01:22 -0100

    Sony eagle device tree after merging yukon into it

---
+++ eagle-vendor.mk
@@ -0,0 +1,155 @@
+PRODUCT_PLATFORM := yukon
+
+# Copyright (C) 2012 Sony Mobile Communication
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_PACKAGES += \
+	btnvtool \
+	hci_qcomm_init \
+	Signedrompatch_v20 \
+	Signedrompatch_v21 \
+	Signedrompatch_v24 \
+	cmnlib_b00 \
+	cmnlib_b01 \
+	cmnlib_b02 \
+	cmnlib_b03 \
+	cmnlib_mdt \
+	cpp_firmware_v1_1_1 \
+	cpp_firmware_v1_1_6 \
+	cpp_firmware_v1_2_0 \
+	tzhdcp_b00 \
+	tzhdcp_b01 \
+	tzhdcp_b02 \
+	tzhdcp_b03 \
+	tzhdcp_mdt \
+	tzlibasb_b00 \
+	tzlibasb_b01 \
+	tzlibasb_b02 \
+	tzlibasb_b03 \
+	tzlibasb_mdt \
+	tzsuntory_b00 \
+	tzsuntory_b01 \
+	tzsuntory_b02 \
+	tzsuntory_b03 \
+	tzsuntory_mdt \
+	venus_b00 \
+	venus_b01 \
+	venus_b02 \
+	venus_b03 \
+	venus_b04 \
+	venus_mbn \
+	venus_mdt \
+	sensors.msm8226 \
+	libyasalgo \
+	libpn547_fw \
+	bt_addr \
+	hvdcp \
+	irsc_util \
+	netmgrd \
+	qmiproxy \
+	qmuxd \
+	qseecomd \
+	rmt_storage \
+	sct_service \
+	ta_qmi_service \
+	tad_static \
+	wcnss_addr \
+	wcnss_service \
+	MTP_Bluetooth_cal \
+	MTP_General_cal \
+	MTP_Global_cal \
+	MTP_Handset_cal \
+	MTP_Hdmi_cal \
+	MTP_Headset_cal \
+	MTP_Speaker_cal \
+	a225_pfp \
+	a225_pm4 \
+	a225p5_pm4 \
+	a300_pfp \
+	a300_pm4 \
+	a330_pfp \
+	a330_pm4 \
+	leia_pfp_470 \
+	leia_pm4_470 \
+	WCNSS_cfg \
+	WCNSS_qcom_cfg \
+	WCNSS_qcom_wlan_nv \
+	libidd \
+	libkeyctrl \
+	libmiscta \
+	libpin-cache \
+	libprotobuf-c \
+	libta \
+	audio_effects \
+	keymaster_b00 \
+	keymaster_b01 \
+	keymaster_b02 \
+	keymaster_b03 \
+	keymaster_mdt \
+	eglsubAndroid \
+	libEGL_adreno \
+	libGLESv1_CM_adreno \
+	libGLESv2_adreno \
+	libq3dtools_adreno \
+	libC2D2 \
+	libOpenCL \
+	libOpenVG \
+	libQSEEComAPI \
+	libRSDriver_adreno \
+	libacdbloader \
+	libacdbmapper \
+	libacdbrtac \
+	libadiertac \
+	libadreno_utils \
+	libadsprpc \
+	libaudcal \
+	libaudioalsa \
+	libbtnv \
+	libc2d2_z180 \
+	libc2d30-a3xx \
+	libc2d30-a4xx \
+	libc2d30 \
+	libdiag \
+	libdrmfs \
+	libdsi_netctrl \
+	libdsnetutils \
+	libdsutils \
+	libgsl \
+	libidl \
+	libloc_api_v02 \
+	libloc_ds_api \
+	libmm-abl-oem \
+	libmm-abl \
+	libmm-color-convertor \
+	libnetmgr \
+	libqcci_legacy \
+	libqdi \
+	libqdp \
+	libqmi \
+	libqmi_cci \
+	libqmi_client_qmux \
+	libqmi_common_so \
+	libqmi_csi \
+	libqmi_csvt_srvc \
+	libqmi_encdec \
+	libqmiservices \
+	libril-qc-qmi-1 \
+	librs_adreno \
+	librs_adreno_sha1 \
+	libsc-a2xx \
+	libsc-a3xx \
+	libtime_genoff \
+	libqcbassboost \
+	libqcreverb \
+	libqcvirt
